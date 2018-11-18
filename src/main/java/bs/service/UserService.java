ae709e2366984def9e7e000e6b472d2bf76a6dd3
Sallbertone <sallbertone@gmail.com>
2018-11-18 18:11:56 +0100

    Add Book and User service layer

---
+++ src/main/java/bs/service/UserService.java
@@ -0,0 +1,19 @@
+package bs.service;
+
+import java.util.List;
+
+import bs.entity.User;
+
+
+public interface UserService
+{
+
+	public List<User> listAllUsers();
+
+	public void saveOrUpdateUser(User user);
+
+	public User getUserById(int id);
+
+	public void deleteUser(int id);
+
+}
