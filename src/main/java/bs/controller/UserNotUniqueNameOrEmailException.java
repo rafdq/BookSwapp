d7ed2bb48d4a9695335bf8263e1f1497270c74de
Sallbertone <sallbertone@gmail.com>
2018-11-24 00:34:47 +0100

    Add UserNotUniqueNameOrEmailException

---
+++ src/main/java/bs/controller/UserNotUniqueNameOrEmailException.java
@@ -0,0 +1,33 @@
+package bs.controller;
+
+public class UserNotUniqueNameOrEmailException extends RuntimeException
+{
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotUniqueNameOrEmailException()
+	{
+	}
+
+	public UserNotUniqueNameOrEmailException(String message)
+	{
+		super(message);
+	}
+
+	public UserNotUniqueNameOrEmailException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public UserNotUniqueNameOrEmailException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public UserNotUniqueNameOrEmailException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
