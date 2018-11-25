6ab11eade89940b9fd1ed75a9965cac8e76d68f1
Sallbertone <sallbertone@gmail.com>
2018-11-26 00:01:40 +0100

    Update listAllUsers in UserDAO  (Page and Pageable) / Add getUsersCount();

--- src/main/java/bs/dao/UserDAO.java
+++ src/main/java/bs/dao/UserDAO.java
@@ -1,19 +1,22 @@
 package bs.dao;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import bs.entity.User;
 
 
-public interface UserDAO
+public interface UserDAO   //extends PagingAndSortingRepository<User, Integer>, CrudRepository<User, Integer>
 {
 
-	public List<User> listAllUsers();
+	public Page<User> listAllUsers(Pageable pageable);
 
 	public void saveOrUpdateUser(User user);
 
 	public User getUserById(int id);
 
 	public void deleteUser(int id);
+
+	public long getUsersCount();
 
 }
