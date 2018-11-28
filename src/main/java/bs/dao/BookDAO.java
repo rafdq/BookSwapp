ec3eff6ca6917b7b39ff93af7be29a4ac8322a55
Sallbertone <sallbertone@gmail.com>
2018-11-28 10:13:51 +0100

    Add listAllActiveBooks ind DAO layer

--- src/main/java/bs/dao/BookDAO.java
+++ src/main/java/bs/dao/BookDAO.java
@@ -2,11 +2,16 @@ package bs.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import bs.entity.Book;
 
 public interface BookDAO
 {
 
+	public Page<Book> listAllActiveBooks(Pageable pageable);
+	
 	public List<Book> listAllBooks();
 
 	public List<Book> findBooks(String searchedText);
--- src/main/java/bs/dao/BookDAOImpl.java
+++ src/main/java/bs/dao/BookDAOImpl.java
@@ -2,10 +2,18 @@ package bs.dao;
 
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import bs.entity.Book;
@@ -23,6 +31,33 @@ public class BookDAOImpl implements BookDAO
 
 	}
 
+	@Override
+	public Page<Book> listAllActiveBooks(Pageable pageable)
+	{
+
+		Session session = getSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<Book> criteriaQuery = builder.createQuery(Book.class);
+		Root<Book> root = criteriaQuery.from(Book.class);
+
+		Predicate condition = builder.equal(root.get("active"), 1);
+		criteriaQuery.where(condition);
+		
+	    criteriaQuery.orderBy(builder.desc(root.get("id")));
+				
+		Query<Book> query = session.createQuery(criteriaQuery);
+
+		query.setFirstResult(pageable.getPageNumber() * pageable.getPageSize());
+		query.setMaxResults(pageable.getPageSize());
+		
+		List<Book> users = query.getResultList();
+		
+		Page<Book> page = new PageImpl<>(users, pageable, getBookCount());
+		
+		return page;
+
+	}
+	
 	public List<Book> listAllBooks()
 	{
 		Query<Book> query = getSession().createQuery("select b from Book b order by b.id desc", Book.class);
@@ -55,6 +90,13 @@ public class BookDAOImpl implements BookDAO
 	public void deleteBook(int id)
 	{
 		getSession().delete(getBookById(id));
+	}
+
+	public long getBookCount()
+	{
+		Query<Long> query = getSession().createQuery("select count(1) from Book", Long.class);
+		
+		return query.getSingleResult().longValue();
 	}
 
 }
